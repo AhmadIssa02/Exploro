4e88126b79e771f0c6252e0e19b85b74e2456bd5
Ahmad Issa <issa.ahmad2002@gmail.com>
2024-04-19 16:53:00 +0300

    Fetching User Information and Displaying FeedPosts

--- exploro/components/ProfileSideBar.tsx
+++ exploro/components/ProfileSideBar.tsx
@@ -1,82 +1,115 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from "next/image";
 import Link from 'next/link';
+import { getTokenCookie, removeTokenCookie } from '@/utils/cookieUtils';
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
 
-type SideBarProps = {
+type User = {
   username: string;
   bio: string;
   profileImageUrl: string;
 };
 
-const ProfileSideBar: React.FC<SideBarProps> = ({ username, bio, profileImageUrl }) => {
-  return <>
-    <Image
-      src={profileImageUrl}
-      alt='profilepic'
-      width={150}
-      height={150}
-      className='mt-10 rounded-full shadow-sm shadow-white'
-      style={{
-        maxWidth: "100%",
-        height: "auto"
-      }}></Image>
-    <div className="flex flex-col items-center space-y-2  ">
-      <span className='text-2xl poppins-bold'>{username}</span>
-    </div>
-    <div className="flex flex-col items-center space-y-2  ">
-      <span className='text-lg poppins-normal'>{bio}</span>
-    </div>
-    <Link href="/userProfile">
-      <button className="flex items-center space-x-2 p-2 border-b-[1.5px] hover:scale-105">
-        <Image
-          src="/images/feed.svg"
-          alt="home"
-          width={19}
-          height={15}
-          className='mt-1'
-          style={{
-            maxWidth: "100%",
-            height: "auto"
-          }} />
-        <span>Your Profile</span>
-      </button>
-    </Link>
-    <Link href="/activity">
-      <button className="flex items-center space-x-2 p-2 border-b-[1.5px] hover:scale-105">
-        <Image
-          src="/images/activity2.svg"
-          alt="home"
-          width={22}
-          height={15}
-          className='mt-1 '
-          style={{
-            maxWidth: "100%",
-            height: "auto"
-          }} />
-        <span>Activity</span>
-      </button>
-    </Link>
-    <Link href="/settings">
-      <button className="flex items-center space-x-2 p-2 border-b-[1.5px] hover:scale-105 ">
-        <Image
-          src="/images/settings4.png"
-          alt="home"
-          width={30}
-          height={35}
-          className='mt-1'
-          style={{
-            maxWidth: "100%",
-            height: "auto"
-          }} />
-        <span>Settings</span>
-      </button>
-    </Link>
-    <div className='pt-'>
-      <button className="text-lg font-semibold p-2 text-center text-black bg-red-500 hover:bg-red-600 rounded-md shadow-sm shadow-red-600 ">
-        <Link href="/auth/login"> Sign Out </Link>
-      </button>
-    </div>
-  </>;
+const ProfileSideBar: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const handleSignOut = () => {
+    removeTokenCookie();
+  };
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const token = getTokenCookie();
+        if (token) {
+          const decoded = jwt.decode(token) as { id: string; }; // Ensure this matches the actual token structure
+          const userId = decoded.id;
+          const response = await axios.get(`http://localhost:3000/users/${userId}`);
+          const userData: User = {
+            username: response.data.name,
+            bio: response.data.bio,
+            profileImageUrl: response.data.profilePicture
+          };
+          setUser(userData);
+        } else {
+          console.error('Token not found.');
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+    fetchUserData();
+  }, []);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <Image
+        src={user.profileImageUrl}
+        alt='profilepic'
+        width={150}
+        height={150}
+        className='mt-10 rounded-full shadow-sm shadow-white'
+        style={{ maxWidth: "100%", height: "auto" }} />
+      <div className="flex flex-col items-center space-y-2">
+        <span className='text-2xl poppins-bold'>{user.username}</span>
+      </div>
+      <div className="flex flex-col items-center space-y-2">
+        <span className='text-lg poppins-normal'>{user.bio}</span>
+      </div>
+      <Link href="/userProfile">
+        <button className="flex items-center space-x-2 p-2 border-b-[1.5px] hover:scale-105">
+          <Image
+            src="/images/feed.svg"
+            alt="home"
+            width={19}
+            height={15}
+            className='mt-1'
+            style={{ maxWidth: "100%", height: "auto" }} />
+          <span>Your Profile</span>
+        </button>
+      </Link>
+      <Link href="/activity">
+        <button className="flex items-center space-x-2 p-2 border-b-[1.5px] hover:scale-105">
+          <Image
+            src="/images/activity2.svg"
+            alt="home"
+            width={22}
+            height={15}
+            className='mt-1 '
+            style={{
+              maxWidth: "100%",
+              height: "auto"
+            }} />
+          <span>Activity</span>
+        </button>
+      </Link>
+      <Link href="/settings">
+        <button className="flex items-center space-x-2 p-2 border-b-[1.5px] hover:scale-105 ">
+          <Image
+            src="/images/settings4.png"
+            alt="home"
+            width={30}
+            height={35}
+            className='mt-1'
+            style={{
+              maxWidth: "100%",
+              height: "auto"
+            }} />
+          <span>Settings</span>
+        </button>
+      </Link>
+      <div className='pt-'>
+        <button className="text-lg font-semibold p-2 text-center text-black bg-red-500 hover:bg-red-600 rounded-md shadow-sm shadow-red-600 " onClick={handleSignOut}>
+          <Link href="/auth/login"> Sign Out </Link>
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default ProfileSideBar;
--- exploro/components/media queries/FeedLg.tsx
+++ exploro/components/media queries/FeedLg.tsx
@@ -7,31 +7,60 @@ import Sidebar from '@/components/SideBar';
 import Header from '@/components/Header'
 import PostInput from '@/components/PostInput';
 import ProfileSideBar from '@/components/ProfileSideBar';
+import Router from 'next/router';
+import { useAuthGuard } from '@/hooks/useAuthGuard';
+import { FeedPostApi } from '@/utils/api/feedPost/feedPost.api';
+import { feedPost } from '@/models/crud';
 
 
-const samplePost = {
-  username: 'Traveler123',
-  bio: 'Traveling the world!',
-  location: 'Mount Everest, Nepal',
-  timeAgo: '3 days ago',
-  content: 'Just reached the base camp, what a view!',
-  profileImageUrl: '/images/man.png',
-  mainImageUrl: '/images/postImage.png',
-  onLike: () => console.log('Liked!'),
-  onComment: () => console.log('Commented!'),
-  onShare: () => console.log('Shared!'),
-};
 
 
 
 const FeedLg: React.FC = () => {
+  useAuthGuard();
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
+  const [posts, setPosts] = useState<feedPost[]>([]);
+  const fetchPosts = async () => {
+    try {
 
+      const postApi = new FeedPostApi()
+      const posts = await postApi.find();
+      setPosts(posts);
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, [])
+
+  const calculateTimeAgo = (date: Date) => {
+
+    const currentDate = new Date();
+    const postDate = new Date(date);
+
+    const diff = currentDate.getTime() - postDate.getTime();
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(diff / (1000 * 60 * 60));
+    const minutes = Math.floor(diff / (1000 * 60));
+    const seconds = Math.floor(diff / 1000);
+
+    if (days > 0) {
+      return `${days}d ago`;
+    } else if (hours > 0) {
+      return `${hours}h ago`;
+    } else if (minutes > 0) {
+      return `${minutes}m ago`;
+    } else {
+      return `${seconds}s ago`;
+    }
+  }
   return (
     <>
       <div className="flex w-full h-100% min-h-screen bg-quarternary-500 text-white font-poppins">
@@ -60,11 +89,7 @@ const FeedLg: React.FC = () => {
 
           {/* Profile Sidebar */}
           <div className={`fixed top-0 right-0 w-1/4 text-lg poppins-semibold h-full space-y-4 bg-primary-700 flex flex-col justify-start items-center z-50  text-white transition-transform transform ${isSidebarOpen ? 'translate-x-0' : 'translate-x-full'}`}>
-            <ProfileSideBar
-              username={samplePost.username}
-              bio={samplePost.bio}
-              profileImageUrl={samplePost.profileImageUrl}
-            />
+            <ProfileSideBar />
           </div>
           {isSidebarOpen && (
             <div className="fixed w-4/5 inset-0 bg-black opacity-40 z-40" onClick={toggleSidebar}></div>
@@ -78,28 +103,21 @@ const FeedLg: React.FC = () => {
             {/* Post Input */}
             <div className="flex flex-col justify-center space-y-6 w-full mr-8">
               <PostInput />
-              <Post
-                username={samplePost.username}
-                location={samplePost.location}
-                timeAgo={samplePost.timeAgo}
-                content={samplePost.content}
-                profileImageUrl={samplePost.profileImageUrl}
-                mainImageUrl={samplePost.mainImageUrl}
-                onLike={samplePost.onLike}
-                onComment={samplePost.onComment}
-                onShare={samplePost.onShare}
-              />
-              <Post
-                username={samplePost.username}
-                location={samplePost.location}
-                timeAgo={samplePost.timeAgo}
-                content={samplePost.content}
-                profileImageUrl={samplePost.profileImageUrl}
-                mainImageUrl={samplePost.mainImageUrl}
-                onLike={samplePost.onLike}
-                onComment={samplePost.onComment}
-                onShare={samplePost.onShare}
-              />
+
+              {posts.map((post, index) => {
+                return <Post
+                  key={index}
+                  username={post.username}
+                  location={post.location}
+                  timeAgo={calculateTimeAgo(post.createdAt)}
+                  content={post.content}
+                  profileImageUrl={post.profileImageUrl}
+                  mainImageUrl={post.mainImageUrl}
+                  onLike={() => console.log('Liked!')}
+                  onComment={() => console.log('Commnet!')}
+                  onShare={() => console.log('Shared!')}
+                />
+              })}
             </div>
 
             {/* Chats */}
