6cbca642df83cd3c36602cc268d09a4e9f4861ef
Ahmad Issa <issa.ahmad2002@gmail.com>
2024-05-06 12:04:34 +0300

    Implementing Securuity Measures

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,62 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from "body-parser";
+import helmet from "helmet";
+import { HttpException, HttpStatus, ValidationError, ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enable("trust proxy");
+  app.set("etag", "strong");
+
+  app.use(
+    bodyParser.json({ limit: "10mb" }),
+    bodyParser.urlencoded({ limit: "10mb", extended: true }),
+  );
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: [`'self'`],
+          styleSrc: [`'self'`, `'unsafe-inline'`],
+          imgSrc: [`'self'`, "data:", "validator.swagger.io"],
+          scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+        },
+      },
+    }),
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidUnknownValues: false,
+      validateCustomDecorators: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      exceptionFactory: (errors: ValidationError[]) =>
+        new HttpException(
+          {
+            status: HttpStatus.BAD_REQUEST,
+            errors: errors.reduce(
+              (accumulator, currentValue) => ({
+                ...accumulator,
+                [currentValue.property]: Object.values(
+                  currentValue.constraints,
+                ).join(", "),
+              }),
+              {},
+            ),
+          },
+          HttpStatus.BAD_REQUEST,
+        ),
+    }),
+  );
+
   app.enableCors(); 
   await app.listen(3000);
 
--- exploro/next.config.mjs
+++ exploro/next.config.mjs
@@ -1,5 +1,32 @@
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    connect-src 'self' http://localhost:3000; 
+    upgrade-insecure-requests;
+`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ' ').trim(), // Ensure proper formatting without newlines
+          },
+        ],
+      },
+    ]
+  },
   reactStrictMode: true,
   images: {
     remotePatterns: [
